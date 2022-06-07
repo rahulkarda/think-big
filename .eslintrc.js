795c2d08d4e4f1c0e67b51e846b19f6b05a75464
Joe Karow <hello@joekarow.dev>
2022-06-07 17:52:27 -0400

    chore(global): :hammer: linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,10 @@
 module.exports = {
   root: true,
   // This tells ESLint to load the config from the package `eslint-config-thinkbig`
-  extends: ['thinkbig'],
+  extends: ["thinkbig"],
   settings: {
     next: {
-      rootDir: ['apps/*/'],
+      rootDir: ["apps/*/"],
     },
   },
 };
